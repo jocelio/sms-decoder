726f1f49c7650a3cfac7af8ed3619a5a427475b6
Jocelio Lima <jocelio.lima@accenture.com>
2017-06-16 14:21:17 -0300

    changes

--- src/sms_decoder/core.clj
+++ src/sms_decoder/core.clj
@@ -2,11 +2,11 @@
   (:gen-class)
   (require [clojure.string :as str]))
 
+(def alphabet (map char (range (int \A) (inc (int \Z)))))
+
+(defn show [message] (seq (.split message "(?<=(.))(?!\\1)")))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (println "Hello, World!"))
-
-(def alphabet (map char (range (int \A) (inc (int \Z)))))
-
-(defn show [message] (str/split message #"(?<=(.))(?!\\1)"))
+  (print (show "333277778" )))
