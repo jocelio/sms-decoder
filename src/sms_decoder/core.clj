e0b00700353bd0f279c9881410aefc0eaa675494
Jocelio Lima <jocelio27@gmail.com>
2017-06-27 19:35:36 -0300

    Codding the solution. Some cases not working yet.

--- src/sms_decoder/core.clj
+++ src/sms_decoder/core.clj
@@ -2,11 +2,22 @@
   (:gen-class)
   (require [clojure.string :as str]))
 
-(def alphabet (map char (range (int \A) (inc (int \Z)))))
+(def alphabet "Returns a list with the alphabet" (map char (range (int \A) (inc (int \Z)))))
 
-(defn show [message] (seq (.split message "(?<=(.))(?!\\1)")))
+(defn gal [x] "Get an alphabet letter in position x" (nth alphabet x))
+
+(defn gfl [x] "Returns the index of the first letter for the given button"
+  (if (>= x 8) (inc (* 3 (- x 2))) (* 3 (- x 2))))
+
+(defn to-int [ch](Integer/parseInt (str ch)))
+
+(defn parse [msg] (str/join (map #(gal %)
+                      (map #(+ (gfl (to-int (first %))) (dec (to-int (second %))))
+                          (map (fn [%] (list (first %) (count %)))
+                              (seq (.split msg "(?<=(.))(?!\\1)")))))))
 
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (print (show "333277778" )))
+  (print (parse "333277778")))
+  ;(print (parse "222 277772")))
+
