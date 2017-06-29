995696debca8a12857c4d75f10755d9f7ce73f6d
Jocelio Lima <jocelio27@gmail.com>
2017-06-28 22:52:33 -0300

    Unit tests

--- test/sms_decoder/core_test.clj
+++ test/sms_decoder/core_test.clj
@@ -2,6 +2,11 @@
   (:require [clojure.test :refer :all]
             [sms-decoder.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest parse-test
+  (testing "Parse sms keypush code"
+    (is (= (parse "333277778") "FAST"))
+    (is (= (parse "11166611144411") "OI"))
+    (is (= (parse "222 277772") "CASA"))
+    (is (= (parse "886207777336683366222 20555666 6642") "UMA SENTENCA LONGA"))
+    (is (= (parse "0123456789") " ADGJMPTW"))
+    (is (= (parse "") ""))))
